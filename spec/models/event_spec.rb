7ee2c7e39ffc80e5a374a3e25e9aaad9bb32e3ee
Simon Fish <sfish@ukcloud.com>
2018-10-09 10:52:23 +0100

    Add tests for Event.all_future

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'when retrieving dates' do
+    let!(:future_event) { FactoryBot.create(:event, datetime: 2.days.after, end_datetime: 3.days.after) }
+    let!(:past_event) { FactoryBot.create(:event, datetime: 2.days.ago, end_datetime: 1.day.ago) }
+    let(:all_future) { described_class.all_future }
+
+    it 'retrieves future dates' do
+      expect(all_future).to include(future_event)
+    end
+
+    it 'does not retrieve past dates' do
+      expect(all_future).not_to include(past_event)
+    end
+  end
 end
