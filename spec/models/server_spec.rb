5adc311c26beac964b23408fe22ac1d4da8698d3
Simon Fish <sfish@ukcloud.com>
2018-10-09 11:08:16 +0100

    Add testing for validation of Server fields

--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Server, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:game) }
+  it { should validate_presence_of(:port) }
+  it { should validate_numericality_of(:port).only_integer }
+  it { should validate_numericality_of(:port).is_less_than_or_equal_to(65535) }
+  it { should validate_numericality_of(:port).is_greater_than_or_equal_to(0) }
 end
