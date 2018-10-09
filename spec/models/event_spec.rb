3b1f2abf21a2f5664465e35c95e03d5811bfc5b5
Simon Fish <sfish@ukcloud.com>
2018-10-09 11:09:54 +0100

    Add tests for Event validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe Event, type: :model do
       expect(all_future).not_to include(past_event)
     end
   end
+
+  context 'validations' do
+    it { should validate_presence_of(:datetime) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_numericality_of(:lan_number).only_integer }
+  end
 end
