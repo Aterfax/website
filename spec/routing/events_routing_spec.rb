429a413934f3bcbbd5d1addfd44ee7a25d28fb59
Simon Fish <sfish@ukcloud.com>
2018-10-16 16:50:55 +0100

    Fix routing tests

--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/events').to route_to('events#index')
+      expect(get: '/admin/events').to route_to('events#index')
     end
 
     it 'routes to #new' do
-      expect(get: '/events/new').to route_to('events#new')
+      expect(get: '/admin/events/new').to route_to('events#new')
     end
 
     it 'routes to #show' do
@@ -17,15 +17,15 @@ RSpec.describe EventsController, type: :routing do
     end
 
     it 'routes to #edit' do
-      expect(get: '/events/1/edit').to route_to('events#edit', id: '1')
+      expect(get: '/admin/events/1/edit').to route_to('events#edit', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/events').to route_to('events#create')
+      expect(post: '/admin/events').to route_to('events#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/events/1').to route_to('events#update', id: '1')
+      expect(put: '/admin/events/1').to route_to('events#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
--- spec/routing/sponsors_routing_spec.rb
+++ spec/routing/sponsors_routing_spec.rb
@@ -5,35 +5,35 @@ require 'rails_helper'
 RSpec.describe SponsorsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/sponsors').to route_to('sponsors#index')
+      expect(get: '/admin/sponsors').to route_to('sponsors#index')
     end
 
     it 'routes to #new' do
-      expect(get: '/sponsors/new').to route_to('sponsors#new')
+      expect(get: '/admin/sponsors/new').to route_to('sponsors#new')
     end
 
     it 'routes to #show' do
-      expect(get: '/sponsors/1').to route_to('sponsors#show', id: '1')
+      expect(get: '/admin/sponsors/1').to route_to('sponsors#show', id: '1')
     end
 
     it 'routes to #edit' do
-      expect(get: '/sponsors/1/edit').to route_to('sponsors#edit', id: '1')
+      expect(get: '/admin/sponsors/1/edit').to route_to('sponsors#edit', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/sponsors').to route_to('sponsors#create')
+      expect(post: '/admin/sponsors').to route_to('sponsors#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/sponsors/1').to route_to('sponsors#update', id: '1')
+      expect(put: '/admin/sponsors/1').to route_to('sponsors#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/sponsors/1').to route_to('sponsors#update', id: '1')
+      expect(patch: '/admin/sponsors/1').to route_to('sponsors#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/sponsors/1').to route_to('sponsors#destroy', id: '1')
+      expect(delete: '/admin/sponsors/1').to route_to('sponsors#destroy', id: '1')
     end
   end
 end
