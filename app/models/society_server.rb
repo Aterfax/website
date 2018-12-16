9d848b81255a3d6b6dbd793aa9e552b2170254f5
Simon Fish <si@mon.fish>
2018-12-16 12:28:32 +0000

    Catch errors in SocietyServer.details, present

--- app/models/society_server.rb
+++ app/models/society_server.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# Represents one of our game servers. Right now, only those hosted on sleipnir
-# are represented.
 require 'steam-condenser'
 
+# Represents one of our game servers. Right now, only those hosted on sleipnir
+# are represented.
 class SocietyServer < ApplicationRecord
   belongs_to :game
   validates :game, presence: true
@@ -11,11 +11,12 @@ class SocietyServer < ApplicationRecord
                    numericality: { greater_than_or_equal_to: 0,
                                    less_than_or_equal_to: 65_535,
                                    only_integer: true }
-  
+
   def details
     SourceServer.new('sleipnir.slugsoc.co.uk', port).server_info
   rescue SteamCondenser::TimeoutError
     nil
+  rescue SteamCondenser::SteamCondenserError
+    nil
   end
-                                   
 end
--- spec/models/society_server_spec.rb
+++ spec/models/society_server_spec.rb
@@ -4,31 +4,32 @@ require 'rails_helper'
 require 'webmock/rspec'
 
 RSpec.describe SocietyServer, type: :model do
+  context 'when the server details are correct' do
+    let(:server_response) do
+      SocietyServer.new(game: Game.new(name: 'GMod'), port: 27_015).details
+    end
 
-  context "when the server details are correct" do
-
-    let(:server_response) { SocietyServer.new(game: Game.new(name: 'GMod'), port: 27015).details } 
-
-    it "returns a hash of data" do
+    it 'returns a hash of data' do
       expect(server_response).to be_a Hash
     end
-    
-    it "returns a hash of data" do
+
+    it 'returns a hash of data' do
       expect(server_response.keys).not_to be_empty
     end
 
-    it "tells how many players are on the server" do
+    it 'tells how many players are on the server' do
       expect(server_response[:number_of_players]).to be_an Integer
     end
 
-    it "tells which map is in rotation" do
+    it 'tells which map is in rotation' do
       expect(server_response[:map_name]).to be_a String
     end
   end
-  
-  context 'when the server cannot be reached' do
 
-    let(:bad_response) { SocietyServer.new(game: Game.new(name: 'Other Game'), port: 12345).details } 
+  context 'when the server cannot be reached' do
+    let(:bad_response) do
+      SocietyServer.new(game: Game.new(name: 'Other Game'), port: 12_345).details
+    end
 
     it 'returns nil' do
       expect(bad_response).to be nil
